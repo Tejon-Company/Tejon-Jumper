06c7bf952fa598abd96b160bb5e1535acae20934
Esteban Suárez Calvo <esteban.suarez.calvo@udc.es>
2025-02-18 12:48:40 +0100

    Rename setup method to _setup for improved encapsulation

--- src/level.py
+++ src/level.py
@@ -10,9 +10,9 @@ class Level:
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
 
-        self.setup(tmx_map)
+        self._setup(tmx_map)
 
-    def setup(self, tmx_map):
+    def _setup(self, tmx_map):
         for x, y, surf in tmx_map.get_layer_by_name("Terrain").tiles():
             Sprite(
                 (x * TILE_SIZE, y * TILE_SIZE),
