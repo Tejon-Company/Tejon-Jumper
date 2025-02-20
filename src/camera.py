31e03a2eb198577fa84fc22ec8654d1272bbc06d
Esteban Suárez Calvo <esteban.suarez.calvo@udc.es>
2025-02-20 18:35:54 +0100

    Refactor camera update methods to separate x and y coordinate handling

--- src/camera.py
+++ src/camera.py
@@ -8,15 +8,20 @@ class Camera:
         self.height = height
 
     def apply(self, entity):
-        return entity.rect.move(self.camera.topleft).move(0, 200)
+        return entity.rect.move(self.camera.topleft)
 
     def update(self, target):
+        self.update_x(target)
+        self.update_y(target)
+
+    def update_x(self, target):
         camera_x = -target.rect.centerx + WINDOW_WIDTH // 2
-        camera_y = -target.rect.centery + WINDOW_HEIGHT // 2
-
         camera_x = min(0, camera_x)
-        camera_y = min(0, camera_y)
         camera_x = max(-(self.width - WINDOW_WIDTH), camera_x)
-        camera_y = max(-(self.height - WINDOW_HEIGHT), camera_y)
+        self.camera.x = camera_x
 
-        self.camera = pygame.Rect(camera_x, camera_y, self.width, self.height)
+    def update_y(self, target):
+        camera_y = -target.rect.centery + WINDOW_HEIGHT // 2
+        camera_y = min(0, camera_y)
+        camera_y = max(-(self.height - WINDOW_HEIGHT), camera_y)
+        self.camera.y = camera_y
