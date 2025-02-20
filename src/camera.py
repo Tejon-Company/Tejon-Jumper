7d6a7db0fbee582c485bcba53e946a80664e73ad
Clauu10 <c.maciel@udc.es>
2025-02-20 12:24:39 +0100

    Created camera.py

---
+++ src/camera.py
@@ -0,0 +1,21 @@
+from settings import *
+
+class Camera:
+    def __init__(self, width, height):
+        self.camera = pygame.Rect(0, 0, width, height)  
+        self.width = width
+        self.height = height
+
+    def apply(self, entity):
+        return entity.rect.move(self.camera.topleft)
+
+    def update(self, target):
+        x = -target.rect.centerx + WINDOW_WIDTH // 2
+        y = -target.rect.centery + WINDOW_HEIGHT // 2
+
+        x = min(0, x) 
+        y = min(0, y)  
+        x = max(-(self.width - WINDOW_WIDTH), x)  
+        y = max(-(self.height - WINDOW_HEIGHT), y)  
+
+        self.camera = pygame.Rect(x, y, self.width, self.height)
