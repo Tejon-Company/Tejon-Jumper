70b695a96f0c78fb188eca83a3179bb2d6fa11e7
Esteban Suárez Calvo <esteban.suarez.calvo@udc.es>
2025-02-20 19:16:51 +0100

    Remove camera y coordiname handling

--- src/camera.py
+++ src/camera.py
@@ -5,23 +5,12 @@ class Camera:
     def __init__(self, width, height):
         self.camera = pygame.Rect(0, 0, width, height)
         self.width = width
-        self.height = height
 
     def apply(self, entity):
         return entity.rect.move(self.camera.topleft)
 
     def update(self, target):
-        self.update_x(target)
-        self.update_y(target)
-
-    def update_x(self, target):
         camera_x = -target.rect.centerx + WINDOW_WIDTH // 2
         camera_x = min(0, camera_x)
         camera_x = max(-(self.width - WINDOW_WIDTH), camera_x)
         self.camera.x = camera_x
-
-    def update_y(self, target):
-        camera_y = -target.rect.centery + WINDOW_HEIGHT // 2
-        camera_y = min(0, camera_y)
-        camera_y = max(-(self.height - WINDOW_HEIGHT), camera_y)
-        self.camera.y = camera_y
