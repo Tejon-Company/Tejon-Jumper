d2570de41b24533ac6a82221c97a9bde1e164023
Clauu10 <c.maciel@udc.es>
2025-02-20 12:25:42 +0100

    Added screen movement

--- src/level.py
+++ src/level.py
@@ -1,6 +1,7 @@
 from settings import *
 from sprites import Sprite
 from player import Player
+from camera import Camera
 
 
 class Level:
@@ -10,7 +11,13 @@ class Level:
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
 
+        map_width = tmx_map.width * TILE_SIZE
+        map_height = tmx_map.height * TILE_SIZE
+
+        self.camera = Camera(map_width, map_height)
+
         self._setup(tmx_map)
+
 
     def _setup(self, tmx_map):
         for x, y, surf in tmx_map.get_layer_by_name("Terrain").tiles():
@@ -22,9 +29,15 @@ class Level:
 
         for object in tmx_map.get_layer_by_name("Objects"):
             if object.name == "Player":
-                Player((object.x, object.y), self.all_sprites, self.collision_sprites)
+                self.player = Player((object.x, object.y), self.all_sprites, self.collision_sprites)
 
     def run(self, delta_time):
         self.all_sprites.update(delta_time)
+        self.camera.update(self.player)
         self.display_surface.fill("black")
-        self.all_sprites.draw(self.display_surface)
+
+        # self.all_sprites.draw(self.display_surface)
+
+        for sprite in self.all_sprites:
+            self.display_surface.blit(sprite.image, self.camera.apply(sprite))
+
