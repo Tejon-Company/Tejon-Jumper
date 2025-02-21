948038e0dd7ef87c036047fda11e32ac0e5ea31e
ines.fperez@udc.es <ines.fperez@udc.es>
2025-02-21 20:52:24 +0100

    Add new parameter speed for different backgrounds and two new methods for updating and drawing it

--- src/background.py
+++ src/background.py
@@ -2,8 +2,19 @@ from settings import *
 
 
 class Background(pygame.sprite.Sprite):
-    def __init__(self, image_path, pos, groups):
+    def __init__(self, image_path, pos, speed, groups):
         super().__init__(groups)
         self.image = pygame.image.load(image_path).convert_alpha()
         self.image = pygame.transform.scale(self.image, (WINDOW_WIDTH, WINDOW_HEIGHT))
         self.rect = self.image.get_rect(topleft=pos)
+        self.speed = speed
+        
+    def update(self, delta_time):
+        self.rect.x -= self.speed * delta_time
+        if self.rect.x <= 0:
+            self.rect.left = 0
+    
+    def draw(self, surface):
+        surface.blit(self.image, self.rect.topleft)
+        if self.rect.right < WINDOW_WIDTH:
+            surface.blit(self.image, (self.rect.right, self.rect.top))
