ad376e1559445d89072b69a39440a3a1cf45da97
ines.fperez@udc.es <ines.fperez@udc.es>
2025-02-24 13:41:33 +0100

    Fix bug in scroll

--- src/scene/background.py
+++ src/scene/background.py
@@ -2,21 +2,25 @@ from settings import *
 
 
 class Background(pygame.sprite.Sprite):
-    def __init__(self, image_path, pos, speed, groups):
+    def __init__(self, image_path, pos, speed, parallax_factor, width, groups):
         super().__init__(groups)
         self.image = pygame.image.load(image_path).convert_alpha()
         self.image = pygame.transform.scale(self.image, (WINDOW_WIDTH, WINDOW_HEIGHT))
         self.rect = self.image.get_rect(topleft=pos)
         self.speed = speed
+        self.parallax_factor = parallax_factor
+        self.width = width
+        self.offset_x = 0
         
     def update(self, delta_time):
-        self.rect.x -= self.speed * delta_time
-        if self.rect.right <= 0:
-            self.rect.left = 0
+        self.offset_x -= self.speed * delta_time
+        
+    def apply_parallax(self, camera_x):
+        self.rect.x = (camera_x * self.parallax_factor) + self.offset_x
 
     def draw(self, surface):
-        
-        # No se está haciendo bien el blit de los distintos fondos
-        surface.blit(self.image, self.rect.topleft)
-        if self.rect.right < WINDOW_WIDTH:
-            surface.blit(self.image, (self.rect.right, self.rect.top))
+        effective_x = self.rect.x % WINDOW_WIDTH
+        surface.blit(self.image, (effective_x - WINDOW_WIDTH, self.rect.y))
+        surface.blit(self.image, (effective_x, self.rect.y))
+        if effective_x < WINDOW_WIDTH:
+            surface.blit(self.image, (effective_x + WINDOW_WIDTH, self.rect.y))
--- src/scene/camera.py
+++ src/scene/camera.py
@@ -7,10 +7,6 @@ class Camera:
 
     def apply(self, entity):
         return entity.rect.move(self.camera.topleft)
-    
-    def apply_parallax(self, background):
-        parallax_factor = background.speed
-        background.rect.x = self.camera.x * parallax_factor
 
     def update(self, target):
         camera_x = -target.rect.centerx + WINDOW_WIDTH // 2
@@ -18,7 +14,11 @@ class Camera:
         camera_x = max(-(self.width - WINDOW_WIDTH), camera_x)
         self.camera.x = camera_x
         
+    def update_background(self, backgrounds, delta_time):
+        for background in backgrounds:
+            background.update(delta_time)
+
     def draw_background(self, backgrounds, surface):
         for background in backgrounds:
-            self.apply_parallax(background)
+            background.apply_parallax(self.camera.x)
             background.draw(surface)
