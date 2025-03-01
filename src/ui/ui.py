7171e0531b7f54feaaa832cf2e9eea7b0e4d88bb
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2025-03-01 18:03:12 +0100

    Add player icon and update lives counter display

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,12 +1,20 @@
 from settings import *
 from os.path import join
+from pygame.transform import scale
+from pygame.image import load
 
 
 class UI:
     def __init__(self, display_surface, player):
         self.display_surface = display_surface
+
         self.player = player
-        self.font = pygame.font.Font(join('assets', 'fonts', 'Beta54.ttf'), 22)
+
+        lives_font_path = join('assets', 'fonts', 'Beta54.ttf')
+        self.font = pygame.font.Font(lives_font_path, 22)
+
+        player_icon_path = join('assets', 'badger_icon.png')
+        self.player_icon = scale(load(player_icon_path), (32, 32))
 
     def draw_hearts(self):
         heart_size = 32
@@ -24,6 +32,7 @@ class UI:
             )
 
     def draw_lifes_counter(self, remaining_lives: int):
-        lives_text = f"Remaining Lives: {remaining_lives}"
+        self.display_surface.blit(self.player_icon, (10, 50))
+        lives_text = f"x{remaining_lives}"
         text_surface = self.font.render(lives_text, True, (255, 255, 255))
-        self.display_surface.blit(text_surface, (10, 50))
+        self.display_surface.blit(text_surface, (50, 50))
