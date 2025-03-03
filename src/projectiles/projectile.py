ff1ea70f41624f30d07df0c69bf48a3b2cadbdd8
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2025-03-03 14:58:10 +0100

    Fix spore movement

--- src/projectiles/projectile.py
+++ src/projectiles/projectile.py
@@ -11,9 +11,11 @@ class Projectile(Sprite, ABC):
         self.is_activated = False
 
     def update(self, platform_rects, delta_time):
+        if not self.is_activated:
+            return
+        self._reset_projectile_if_off_screen()
         self.old_rect = self.rect.copy()
         self._move(delta_time)
-        self._reset_projectile_if_off_screen()
 
     def change_position(self, new_pos_x, new_pos_y):
         self.rect.x = new_pos_x
--- src/projectiles/spore.py
+++ src/projectiles/spore.py
@@ -11,12 +11,11 @@ class Spore(Projectile):
         self.initial_pos = 0
 
     def _move(self, delta_time):
-        self.rect.x += self.direction.x * self.speed * delta_time
+        if self.is_activated:
+            self.rect.x += self.direction.x * self.speed * delta_time
 
     def _reset_projectile_if_off_screen(self):
         traveled_distance = abs(self.rect.x - self.initial_pos)
-        print(traveled_distance)
-
-        if traveled_distance > 1 * TILE_SIZE:
-            self.initial_pos = 0
+        if traveled_distance > 3 * TILE_SIZE:
+            print(f"rect x: {self.rect.x}    Initial position: {self.initial_pos}")
             self.is_activated = False
