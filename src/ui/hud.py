d182fadef074a1e4a4330376d4d7c2d6ba063e10
Clauu10 <c.maciel@udc.es>
2025-03-05 12:31:51 +0100

    Added energy bar to the hud

--- src/ui/hud.py
+++ src/ui/hud.py
@@ -17,6 +17,7 @@ class HUD:
     def draw_hud(self, remaining_health_points: int, remaining_lives: int):
         self._draw_hearts(remaining_health_points)
         self._draw_lifes_counter(remaining_lives)
+        self._draw_energy_bar(100)
 
     def _draw_hearts(self, remaining_health_points):
         heart_size = 32
@@ -38,3 +39,16 @@ class HUD:
         lives_text = f"x{remaining_lives}"
         text_surface = self.font.render(lives_text, True, (255, 255, 255))
         self.display_surface.blit(text_surface, (50, 50))
+
+    def _draw_energy_bar(self, energy: float):
+            bar_width = 150
+            bar_height = 15
+            start_x = 10
+            start_y = 90  
+            border_color = (255, 255, 255)
+            fill_color = (255, 255, 0) 
+
+            pygame.draw.rect(self.display_surface, border_color, (start_x, start_y, bar_width, bar_height), 2)
+
+            energy_width = int((energy / 100) * (bar_width - 4))  
+            pygame.draw.rect(self.display_surface, fill_color, (start_x + 2, start_y + 2, energy_width, bar_height - 4))
