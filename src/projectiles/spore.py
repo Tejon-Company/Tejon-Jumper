d110b189e578ffff81d662ef8d7e9ff319115ecc
CarolinaRey03 <carolina.rey@udc.es>
2025-03-06 17:14:22 +0100

    Refactor off-screen detection logic for projectiles and improve readability (bug fixed)

--- src/projectiles/spore.py
+++ src/projectiles/spore.py
@@ -14,12 +14,27 @@ class Spore(Projectile):
         self.rect.x += self.direction.x * self.speed * delta_time
 
     def _reset_projectile_if_off_screen(self):
-        distance_traveled = self.initial_x_pos + self.max_distance
-        is_off_screen = self.rect.x <= distance_traveled
-        if is_off_screen:
-            self.is_activated = False
-            self.rect.x = -100
-            self.rect.y = -100
+        if self._is_projectile_off_screen():
+            self._deactivate_projectile()
+
+    def _is_projectile_off_screen(self):
+        distance_traveled = self._calculate_max_travel_distance()
+        return self._check_position_exceeds_distance(distance_traveled)
+
+    def _calculate_max_travel_distance(self):
+        if self.direction.x > 0:
+            return self.initial_x_pos + self.max_distance
+        return self.initial_x_pos - self.max_distance
+
+    def _check_position_exceeds_distance(self, distance_traveled):
+        if self.direction.x > 0:
+            return self.rect.x >= distance_traveled
+        return self.rect.x <= distance_traveled
+
+    def _deactivate_projectile(self):
+        self.is_activated = False
+        self.rect.x = -100
+        self.rect.y = -100
 
     def change_position(self, new_pos_x, new_pos_y):
         self.rect.x = new_pos_x
