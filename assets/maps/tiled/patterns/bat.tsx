a8b5bbb2cf358f79c4e796eec97193b910a8a3dc
ines.fperez@udc.es <ines.fperez@udc.es>
2025-03-08 14:40:29 +0100

    Add bat to the level

---
+++ assets/maps/tiled/patterns/bat.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="bat" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="../../../creatures_and_else/enemies/without_background/bat.png" width="65" height="32"/>
+</tileset>
