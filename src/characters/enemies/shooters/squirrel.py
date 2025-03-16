394501270737326c3b643018eeddd718e1ded613
Esteban Suárez Calvo <114094577+estebansuarezcalvoudc@users.noreply.github.com>
2025-03-16 13:46:59 +0100

    Add shoot direction to squirrels (#117)

--- src/characters/enemies/shooters/squirrel.py
+++ src/characters/enemies/shooters/squirrel.py
@@ -11,3 +11,26 @@ class Squirrel(Shooter):
         self.rect = self.image.get_frect(topleft=pos)
 
         self._setup_animation()
+
+        self._is_facing_right = False
+
+    def update(self, delta_time):
+        super().update(delta_time)
+        self._update_direction()
+
+    def _update_direction(self):
+        self._is_facing_right = self.player.rect.x > self.rect.x
+
+        if self._is_facing_right:
+            self.image = pygame.transform.flip(self.image, True, False)
+
+    def _shoot(self):
+        current_time = pygame.time.get_ticks()
+        cooldown_passed = current_time - self.last_shot >= self.shoot_cooldown
+        is_shooting = cooldown_passed and self._is_player_near()
+
+        if is_shooting:
+            self.is_shooting = True
+            self.projectiles_pool.shoot(
+                self.pos[0], self.pos[1], self._is_facing_right)
+            self.last_shot = current_time
