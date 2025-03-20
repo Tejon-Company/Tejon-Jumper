9848f6355779f6d4714a99b76c231972a5f36424
Clauu10 <114505910+Clauu10@users.noreply.github.com>
2025-03-20 15:50:21 +0100

    TJ-58 Añadir menú de inicio (#135)
    * Created main menu
    
    * Refactor settings menu layout
    
    * Refactor mouse button handling
    
    * Fixed game pause
    
    * Added settings menu
    
    * Update font styles
    
    * Refactor settings menu

--- src/game.py
+++ src/game.py
@@ -128,14 +128,16 @@ class Game:
         if not has_max_health and should_receive_heal:
             self.health_points += 1
 
-    def change_current_level(self):
-        if self.current_level == 3:
-            self.current_level = 1
-            return
+    def _is_game_paused(self):
+        keys = pygame.key.get_just_released()
 
-        self.current_level += 1
-        self._load_level()
+        if keys[pygame.K_p]:
+            self.is_on_pause = not self.is_on_pause
+            self.director.stack_scene(Pause(self.director))
+            self.is_on_pause = False
 
+        return self.is_on_pause
+    
     def draw(self, surface):
         self.level.draw(surface)
         self.hud.draw_hud(self.health_points,
