85d6c24cba9e5676489eb21997dd66c039934906
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2025-03-21 21:49:11 +0100

    Adjust mushroom jumping in level 3

--- src/scene/menu.py
+++ src/scene/menu.py
@@ -34,6 +34,7 @@ class Menu(Scene):
     def _mouse_button_down(self, event):
         if self.play_btn.collidepoint(event.pos):
             from game import Game
+
             self.director.change_scene(Game(self.director))
 
         if self.settings_btn.collidepoint(event.pos):
@@ -43,15 +44,14 @@ class Menu(Scene):
         display_surface.fill((34, 139, 34))
         display_surface.blit(self.logo, self.logo_rect.topleft)
 
-        font = ResourceManager.load_font('Timetwist-Regular.ttf', 22)
+        font = ResourceManager.load_font("Timetwist-Regular.ttf", 22)
 
         self._draw_button(display_surface, self.play_btn, "Play", font)
-        self._draw_button(display_surface, self.settings_btn,
-                          " Settings", font)
-        
+        self._draw_button(display_surface, self.settings_btn, " Settings", font)
+
     def _draw_button(self, display_surface, rect, text, font):
-        draw_rect(display_surface, 'black', rect)
-        text_surf = font.render(text, True, 'white')
+        draw_rect(display_surface, "black", rect)
+        text_surf = font.render(text, True, "white")
         text_rect = text_surf.get_rect(center=rect.center)
         display_surface.blit(text_surf, text_rect.topleft)
 
