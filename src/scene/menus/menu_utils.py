5d065c1503db3fe151e783717eb6412832c5629d
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2025-03-22 02:07:27 +0100

    Modify the SettingsMenu to look better

--- src/scene/menus/menu_utils.py
+++ src/scene/menus/menu_utils.py
@@ -1,4 +1,6 @@
+import pygame
 from pygame.draw import rect as draw_rect
+from resource_manager import ResourceManager
 
 
 def draw_button(display_surface, rect, text, font):
@@ -10,3 +12,49 @@ def draw_button(display_surface, rect, text, font):
 
 def display_background(display_surface, background):
     display_surface.blit(background, (0, 0))
+
+
+def display_label(display_surface, text, x, y, font):
+    label = font.render(text, True, "white")
+    padding = 4
+    label_width = label.get_width() + padding * 2
+    label_height = label.get_height() + padding * 2
+    label_bg = pygame.Surface((label_width, label_height), pygame.SRCALPHA)
+    rect = label_bg.get_rect()
+    pygame.draw.rect(label_bg, "#9A7856", rect, border_radius=5)
+    display_surface.blit(label_bg, (x - padding, y - padding))
+    display_surface.blit(label, (x, y))
+
+
+def draw_volume_bar(display_surface, rect, volume, text, font):
+    volume = _update_volume_from_mouse(rect, volume, text)
+    _draw_bar(display_surface, rect, volume)
+    text_width = font.render(text, True, "white").get_width()
+    display_label(display_surface, text, rect.x - text_width - 10, rect.y, font)
+
+
+def _update_volume_from_mouse(rect, volume, text):
+    if not (
+        pygame.mouse.get_pressed()[0] and rect.collidepoint(pygame.mouse.get_pos())
+    ):
+        return volume
+
+    mouse_x = pygame.mouse.get_pos()[0]
+    new_volume = max(0, min((mouse_x - rect.x) / rect.width, 1))
+
+    if text == "Music":
+        ResourceManager.set_music_volume(new_volume)
+    else:
+        ResourceManager.set_effects_volume(new_volume)
+
+    return new_volume
+
+
+def _draw_bar(display_surface, rect, volume):
+    draw_rect(display_surface, "white", rect, border_radius=10)
+    draw_rect(
+        display_surface,
+        "#90D5FF",
+        (rect.x, rect.y, volume * rect.width, rect.height),
+        border_radius=10,
+    )
--- src/scene/menus/settings_menu.py
+++ src/scene/menus/settings_menu.py
@@ -1,15 +1,16 @@
-from pygame.draw import rect as draw_rect
 from pygame.rect import Rect
 from scene.scene import Scene
 from resource_manager import ResourceManager
 from settings import *
 import pygame
+from scene.menus.menu_utils import *
 
 
 class SettingsMenu(Scene):
     def __init__(self, director):
         super().__init__(director)
         self.display_surface = pygame.display.get_surface()
+        self.background = ResourceManager.load_image("menu_background.jpeg")
 
         X = WINDOW_WIDTH // 2
         Y = WINDOW_HEIGHT // 2
@@ -17,10 +18,10 @@ class SettingsMenu(Scene):
         HEIGHT = 50
         VOLUME_BAR_HEIGHT = 20
 
-        self.music_volume_bar = Rect(
-            X - WIDTH // 2, Y - 150, WIDTH, VOLUME_BAR_HEIGHT)
+        self.music_volume_bar = Rect(X - WIDTH // 2, Y - 150, WIDTH, VOLUME_BAR_HEIGHT)
         self.effects_volume_bar = Rect(
-            X - WIDTH // 2, Y - 100, WIDTH, VOLUME_BAR_HEIGHT)
+            X - WIDTH // 2, Y - 100, WIDTH, VOLUME_BAR_HEIGHT
+        )
 
         self.difficulty_btn = Rect(X - WIDTH // 2, Y - 30, WIDTH, HEIGHT)
         self.resolution_btn = Rect(X - WIDTH // 2, Y + 50, WIDTH, HEIGHT)
@@ -35,6 +36,8 @@ class SettingsMenu(Scene):
         self.resolutions = [(800, 600), (1280, 720), (1920, 1080)]
         self.current_resolution = 1
 
+        self.font = ResourceManager.load_font("Timetwist-Regular.ttf", 20)
+
     def events(self, events_list):
         for event in events_list:
             if event.type == pygame.QUIT:
@@ -45,58 +48,65 @@ class SettingsMenu(Scene):
     def _mouse_button_down(self, event):
         if self.music_volume_bar.collidepoint(event.pos):
             self.music_volume = (
-                event.pos[0] - self.music_volume_bar.x) / self.music_volume_bar.width
+                event.pos[0] - self.music_volume_bar.x
+            ) / self.music_volume_bar.width
             ResourceManager.set_music_volume(self.music_volume)
 
         elif self.effects_volume_bar.collidepoint(event.pos):
             self.effects_volume = (
-                event.pos[0] - self.effects_volume_bar.x) / self.effects_volume_bar.width
+                event.pos[0] - self.effects_volume_bar.x
+            ) / self.effects_volume_bar.width
             ResourceManager.set_effects_volume(self.effects_volume)
 
         elif self.difficulty_btn.collidepoint(event.pos):
-            self.current_difficulty = (
-                self.current_difficulty + 1) % len(self.difficulty_levels)
+            self.current_difficulty = (self.current_difficulty + 1) % len(
+                self.difficulty_levels
+            )
 
         elif self.back_btn.collidepoint(event.pos):
             from scene.menus.main_menu import MainMenu
+
             self.director.change_scene(MainMenu(self.director))
 
     def draw(self, display_surface):
-        display_surface.fill((50, 50, 50))
-        font = ResourceManager.load_font('Timetwist-Regular.ttf', 20)
+        display_background(display_surface, self.background)
 
-        self._draw_volume_bar(
-            display_surface, self.music_volume_bar, self.music_volume, "Music", font)
-        self._draw_volume_bar(display_surface, self.effects_volume_bar,
-                              self.effects_volume, "Sound Effects", font)
+        draw_volume_bar(
+            display_surface,
+            self.music_volume_bar,
+            self.music_volume,
+            "Music",
+            self.font,
+        )
+        draw_volume_bar(
+            display_surface,
+            self.effects_volume_bar,
+            self.effects_volume,
+            "Sound Effects",
+            self.font,
+        )
 
-        game_mode_label = font.render("Game Mode", True, 'white')
-        display_surface.blit(
-            game_mode_label, (self.difficulty_btn.x - 150, self.difficulty_btn.y + 15))
+        display_label(
+            display_surface,
+            "Game Mode",
+            self.difficulty_btn.x - 150,
+            self.difficulty_btn.y + 15,
+            self.font,
+        )
+        draw_button(
+            display_surface,
+            self.difficulty_btn,
+            f"{self.difficulty_levels[self.current_difficulty]}",
+            self.font,
+        )
 
-        self._draw_button(display_surface, self.difficulty_btn,
-                          f"{self.difficulty_levels[self.current_difficulty]}", font)
-
-        resolution_label = font.render("Resolution", True, 'white')
-        display_surface.blit(
-            resolution_label, (self.resolution_btn.x - 135, self.resolution_btn.y + 15))
-
+        display_label(
+            display_surface,
+            "Resolution",
+            self.resolution_btn.x - 135,
+            self.resolution_btn.y + 15,
+            self.font,
+        )
         resolution_text = f"{self.resolutions[self.current_resolution][0]}x{self.resolutions[self.current_resolution][1]}"
-        self._draw_button(display_surface, self.resolution_btn,
-                          resolution_text, font)
-
-        self._draw_button(display_surface, self.back_btn, "Return", font)
-
-    def _draw_volume_bar(self, display_surface, rect, volume, label, font):
-        draw_rect(display_surface, 'white', rect)
-        draw_rect(display_surface, 'green',
-                  (rect.x, rect.y, volume * rect.width, rect.height))
-        label_surf = font.render(label, True, 'white')
-        display_surface.blit(
-            label_surf, (rect.x - label_surf.get_width() - 10, rect.y))
-
-    def _draw_button(self, display_surface, rect, text, font):
-        draw_rect(display_surface, 'black', rect)
-        text_surf = font.render(text, True, 'white')
-        text_rect = text_surf.get_rect(center=rect.center)
-        display_surface.blit(text_surf, text_rect.topleft)
+        draw_button(display_surface, self.resolution_btn, resolution_text, self.font)
+        draw_button(display_surface, self.back_btn, "Return", self.font)
