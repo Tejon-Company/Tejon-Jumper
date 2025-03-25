d78c0ecccd50df7012e2c09d0a1f1bbc49c1ccef
CarolinaRey03 <carolina.rey@udc.es>
2025-03-25 15:17:39 +0100

    Update background images and remove scaling in Background class

--- src/scene/background.py
+++ src/scene/background.py
@@ -6,9 +6,6 @@ class Background:
     def __init__(self, image_path, pos, parallax_factor):
         self.settings = Settings()
         self.image = pygame.image.load(image_path).convert_alpha()
-        self.image = pygame.transform.scale(
-            self.image, (self.settings.window_width, self.settings.window_height)
-        )
         self.rect = self.image.get_rect(topleft=pos)
         self.parallax_factor = parallax_factor
         self.offset_x = 0
