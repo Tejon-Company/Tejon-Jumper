5fb3864023891fd02f1bdc7eb8cb58970566a8ef
Tejon13 <m.tejon@udc.es>
2025-03-26 18:35:00 +0100

    start level1 full hd

---
+++ assets/full_hd/tiled/patterns/all_entities.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="all_entities" tilewidth="48" tileheight="48" tilecount="23" columns="23">
+ <image source="../../sprite_sheets/day/all_entities.png" width="1104" height="48"/>
+</tileset>
